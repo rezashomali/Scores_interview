a8ee843d6b51da0efecc449a8600da4affc2ded5
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-11-09 18:49:23 +0100

    change SecondaryListItems to function

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import AssignmentIcon from '@material-ui/icons/Assignment';
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import AssignmentIcon from "@material-ui/icons/Assignment";
 
 export const mainListItems = (
   <div>
@@ -17,16 +17,16 @@ export const mainListItems = (
   </div>
 );
 
-export const secondaryListItems = (
+export const SecondaryListItems = ({ selectChartDataType }) => (
   <div>
     <ListSubheader inset>Saved reports</ListSubheader>
-    <ListItem button>
+    <ListItem button onClick={() => selectChartDataType("gender")}>
       <ListItemIcon>
         <AssignmentIcon />
       </ListItemIcon>
       <ListItemText primary="Scores by gender" />
     </ListItem>
-    <ListItem button>
+    <ListItem button onClick={() => selectChartDataType("country")}>
       <ListItemIcon>
         <AssignmentIcon />
       </ListItemIcon>
