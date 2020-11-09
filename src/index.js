eed5ce2b565b2122af8dc992c31f5f11369de420
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-11-09 16:13:05 +0100

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-// import { ThemeProvider } from "@material-ui/styles";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  // <ThemeProvider>
   <>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
   </>,
-  // </ThemeProvider>,
   document.querySelector("#root")
 );
 
