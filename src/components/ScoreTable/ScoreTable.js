985374392e20ed4b851c0eb2ab177e23f1e43adc
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-11-10 13:59:44 +0100

    add sorting

--- src/components/ScoreTable/ScoreTable.js
+++ src/components/ScoreTable/ScoreTable.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-script-url */
 
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import {
+  Table,
+  Button,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
 import Title from "../Title/Title";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 
 const useStyles = makeStyles((theme) => ({
   seeMore: {
@@ -19,6 +24,35 @@ const useStyles = makeStyles((theme) => ({
 export default function ScoreTable({ userData }) {
   const classes = useStyles();
   const [readmore, setReadmore] = useState(false);
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: null,
+  });
+
+  let sortedData = userData;
+  useMemo(() => {
+    if (sortConfig.key !== null) {
+      sortedData.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+
+    return sortedData;
+  }, [sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
 
   return (
     <React.Fragment>
@@ -26,16 +60,130 @@ export default function ScoreTable({ userData }) {
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Last name</TableCell>
-            <TableCell>First name</TableCell>
-            <TableCell>Gender</TableCell>
-            <TableCell>City</TableCell>
-            <TableCell>Country</TableCell>
-            <TableCell align="right">Score</TableCell>
+            <TableCell size="small" padding="none">
+              <Button
+                type="button"
+                size="small"
+                endIcon={
+                  sortConfig.key === "last_name" ? (
+                    sortConfig.direction === "ascending" ? (
+                      <ArrowDownwardIcon />
+                    ) : (
+                      <ArrowUpwardIcon />
+                    )
+                  ) : (
+                    ""
+                  )
+                }
+                onClick={() => requestSort("last_name")}
+              >
+                Last name
+              </Button>
+            </TableCell>
+            <TableCell size="small" padding="none">
+              <Button
+                type="button"
+                size="small"
+                endIcon={
+                  sortConfig.key === "first_name" ? (
+                    sortConfig.direction === "ascending" ? (
+                      <ArrowDownwardIcon />
+                    ) : (
+                      <ArrowUpwardIcon />
+                    )
+                  ) : (
+                    ""
+                  )
+                }
+                onClick={() => requestSort("first_name")}
+              >
+                First name
+              </Button>
+            </TableCell>
+            <TableCell size="small" padding="none">
+              <Button
+                size="small"
+                type="button"
+                endIcon={
+                  sortConfig.key === "gender" ? (
+                    sortConfig.direction === "ascending" ? (
+                      <ArrowDownwardIcon />
+                    ) : (
+                      <ArrowUpwardIcon />
+                    )
+                  ) : (
+                    ""
+                  )
+                }
+                onClick={() => requestSort("gender")}
+              >
+                Gender
+              </Button>
+            </TableCell>
+            <TableCell size="small" padding="none">
+              <Button
+                size="small"
+                type="button"
+                endIcon={
+                  sortConfig.key === "city" ? (
+                    sortConfig.direction === "ascending" ? (
+                      <ArrowDownwardIcon />
+                    ) : (
+                      <ArrowUpwardIcon />
+                    )
+                  ) : (
+                    ""
+                  )
+                }
+                onClick={() => requestSort("city")}
+              >
+                City
+              </Button>
+            </TableCell>
+            <TableCell size="small" padding="none">
+              <Button
+                size="small"
+                type="button"
+                endIcon={
+                  sortConfig.key === "country" ? (
+                    sortConfig.direction === "ascending" ? (
+                      <ArrowDownwardIcon />
+                    ) : (
+                      <ArrowUpwardIcon />
+                    )
+                  ) : (
+                    ""
+                  )
+                }
+                onClick={() => requestSort("country")}
+              >
+                Country
+              </Button>
+            </TableCell>
+            <TableCell align="right" size="small" padding="none">
+              <Button
+                size="small"
+                type="button"
+                endIcon={
+                  sortConfig.key === "score" ? (
+                    sortConfig.direction === "ascending" ? (
+                      <ArrowDownwardIcon />
+                    ) : (
+                      <ArrowUpwardIcon />
+                    )
+                  ) : (
+                    ""
+                  )
+                }
+                onClick={() => requestSort("score")}
+              >
+                Score
+              </Button>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {userData.slice(0, readmore ? -1 : 10).map((row) => (
+          {sortedData.slice(0, readmore ? -1 : 10).map((row) => (
             <TableRow key={row.id}>
               <TableCell>{row.last_name}</TableCell>
               <TableCell>{row.first_name}</TableCell>
