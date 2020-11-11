20fc19d73407a493b45163bbeb8717a1d472f90c
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-11-11 11:43:38 +0100

    build project

---
+++ build/precache-manifest.f7d4fe372980f41fd031eb60ded5f5dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3739f58f33bada9a6d3a83a0ec4c9801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfece25803ea3ef3202e",
+    "url": "/static/css/main.866b4e6a.chunk.css"
+  },
+  {
+    "revision": "a59e9c2e0f79bfedabdc",
+    "url": "/static/js/2.136e00c0.chunk.js"
+  },
+  {
+    "revision": "cfece25803ea3ef3202e",
+    "url": "/static/js/main.c68a53e6.chunk.js"
+  },
+  {
+    "revision": "fd247486a93147c6851e",
+    "url": "/static/js/runtime~main.b41793ee.js"
+  }
+]);
